e359ff0336208eb1704463e0be024d193a31d792
padhan3231 <padhan3231@gmail.com>
2026-07-14 18:19:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -701,7 +701,8 @@ async def upload(bot: Client, m: Message):
             if "jw-prod" in url:
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
             elif "webvideos.classplusapp." in url or "classplusapp" in url:
-                cmd = f'yt-dlp --add-header "referer:https://web.classplusapp.com/" --add-header "x-cdn-tag:empty" --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" -f "{ytf}" "{url}" -o "{name}.mp4"'
+    # Yahan "x-access-token" add kiya gaya hai jisme chat se liya hua raw_text4 (aapka token) jayega
+    cmd = f'yt-dlp --add-header "x-access-token:{str(raw_text4)}" --add-header "referer:https://web.classplusapp.com/" --add-header "x-cdn-tag:empty" --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" -f "{ytf}" "{url}" -o "{name}.mp4"'
             elif "youtube.com" in url or "youtu.be" in url:
                 cmd = f'yt-dlp --cookies youtube_cookies.txt -f "{ytf}" "{url}" -o "{name}".mp4'
             else:
