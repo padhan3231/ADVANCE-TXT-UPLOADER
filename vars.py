0a8eb4ff85678a86429a3fa998a4834cd6a9b64e
padhan3231 <padhan3231@gmail.com>
2026-07-15 21:41:15 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,8 +1,8 @@
-# Don't Remove Credit Tg - @Tushar0125
-# Ask Doubt on telegram @Tushar0125
-
 from os import environ
 
-API_ID = int(environ.get("API_ID", "23237831")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "017dc661ca1432ac2fe5ecb62499d88f") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "23237831"))
+API_HASH = environ.get("API_HASH", "017dc661ca1432ac2fe5ecb62499d88f")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
+# Nayi line jo aapko jodni hai:
+CLASSPLUS_TOKEN = environ.get("CLASSPLUS_TOKEN", "yahan_default_token_daal_sakte_hain")
